afc27a25e2438a874e1c40f8352743b3716b97c5
David Proaño <52711582+gdproano@users.noreply.github.com>
2021-05-21 11:35:39 -0500

    add rules

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -14,7 +14,22 @@ export default function App() {
         source={{
           uri: 'https://reactnative.dev/img/tiny_logo.png',
         }} />
-      <Text style={styles.text}>Welcome to the 101 React Native</Text>
+      <Text style={styles.text}>Alinear los elementos de texto horizontalmente</Text>
+      <Text style={styles.text}>Centrar la imagen</Text>
+      <Text style={styles.text}>Distribuir el espacio entre los elementos</Text>
+      <Text style={styles.text}>Enviar el botón a la parte inferior</Text>
+      <Text style={styles.text}>Cambiar el orden de los elementos</Text>
+      <Text style={styles.text}>Element 1</Text>
+      <Text style={styles.text}>Element 2</Text>
+      <Text style={styles.text}>Element 3</Text>
+      <Text style={styles.text}>Element 4</Text>
+      <Text style={styles.text}>Element 5</Text>
+      <Button
+        onPress={() => {}}
+        title="Touch me"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -24,8 +39,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fafafa',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   text: {
     color: 'blue',
